e6dd65f7a0920fc4a088339a986d0bd54941b833
Kuzmichou Aliaksei <kuzmichoualiaksei@gmail.com>
2021-10-12 22:39:17 +0300

    SELECT 0.0.1

--- src/main/java/com/example/kuzmichev/testsql/TestSqlApplication.java
+++ src/main/java/com/example/kuzmichev/testsql/TestSqlApplication.java
@@ -11,3 +11,17 @@ public class TestSqlApplication {
     }
 
 }
+
+
+//      - Получить количество неактивных (не было сессий) клиентов.
+//      Вариант 1
+//      SELECT count (USERID) from USERS
+//      where USERID not in(SELECT distinct USERID from USERSESSIONS )
+
+//      Вариант 2
+//      SELECT COUNT (*)
+//      FROM USERSESSIONS  t
+//      RIGHT JOIN USERS c on t.USERID=c.USERID
+//      WHERE t.USERID is NULL
+
+
