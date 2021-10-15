adfffde9c1cf2056f3ce6c1256522a9cd8c9f197
Aliaksei Kuzmichou <kuzmichoualiaksei@gmail.com>
2021-10-15 07:37:44 +0300

    release 1.0.1

--- src/main/java/com/example/kuzmichev/testsql/TestSqlApplication.java
+++ src/main/java/com/example/kuzmichev/testsql/TestSqlApplication.java
@@ -11,32 +11,3 @@ public class TestSqlApplication {
     }
 
 }
-
-
-//      - Получить количество неактивных (не было сессий) клиентов.
-//      Вариант 1
-//      SELECT COUNT (clientid ) FROM users
-//      WHERE userid NOT IN (SELECT DISTINCT userid FROM usersessions)
-
-//      Вариант 2
-//      SELECT COUNT (clientid)
-//      FROM usersessions t
-//      RIGHT JOIN users c ON t.userid=c.userid
-//      WHERE t.userid IS NULL
-
-//      - Получить список уникальных UserId активных пользователей, которые не пользовались каналом 1.
-//      SELECT DISTINCT userid
-//      FROM usersessions WHERE channeltype NOT IN (1)
-
-//      - Получить максимальное UserId активного пользователя, статус которого также не равен 1.
-//      SELECT MAX(t.userid)
-//      FROM usersessions t
-//      JOIN users c ON t.userid=c.userid AND c.status NOT IN (1)
-
-//      - Получить список количества сессий с разделением на MobAppVersion. То есть список, сгруппированный по MobAppVersion,
-//      в котором для каждой из MobAppVersion будет подсчитано количество сессий со "старой" mobosversion,
-//      "старой" версией считается версия ниже 80 или неуказанная
-//       SELECT COUNT (sessionid) AS Session_count,  mobappversion, mobosversion
-//       FROM usersessions t
-//       WHERE t.mobosversion  IS NULL  OR t.mobosversion < 80
-//       GROUP BY mobappversion
